8f64bafea8d75ba078e9a4517b77f504ac86a5cc
Philipocoding <philipgraham_1@hotmail.co.uk>
2026-02-05 21:30:40 +0000

    cors file added

--- supabase/functions/invite-student/index.ts
+++ supabase/functions/invite-student/index.ts
@@ -5,6 +5,7 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 
 //Deno is the open-source JavaScript runtime for the modern web.
+import { corsHeaders } from '../_shared/cors.ts'
 import "@supabase/functions-js/edge-runtime.d.ts"
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts" //serve is deno built in web server
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7'
