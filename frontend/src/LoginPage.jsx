b573d29c4feb00d601af6af71fdd556957eb4531
Philipocoding <philipgraham_1@hotmail.co.uk>
2026-02-12 20:47:29 +0000

    .

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Mail, Lock, LogIn, ShieldCheck, UserPlus, ArrowRight } from 'lucide-react';
-
+import { useState, useEffect } from 'react';
 
 const INPUT_CONTAINER = "relative mb-1 w-full";
 const ICON_STYLE = "absolute left-3 top-1/2 -translate-y-1/2 text-gray-400";
